a9ca841318355cb330025a131c8a65c9969d5894
your_name <your_address_email@example.com>
2023-02-22 11:01:15 +0100

    Modificado

--- Entities/RegisterProduct.cs
+++ Entities/RegisterProduct.cs
@@ -9,6 +9,6 @@ namespace Entities
     public class RegisterProduct : Entity
     {
         public string Name { get; set; }
-        public string Price { get; set; }
+        public string Price { get; set; } 
     }
 }
